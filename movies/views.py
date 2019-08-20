4c1b539ce58bc1fd231a40c991735d656ed86729
thuitafaith <thuitamuthoni15@gmail.com>
2019-08-20 11:55:44 +0300

    create urls file

--- movies/views.py
+++ movies/views.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
